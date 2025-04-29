e8e8c32b3cd54f45f2f854f31bf02c802e4ce3d9
VVPCode <pirotivitor3@gmail.com>
2025-04-28 23:09:49 -0300

    Criação do DockerFile; Criação do config.py; Alterações no app.py para funcionar junto com o config.py

--- app.py
+++ app.py
@@ -2,14 +2,10 @@ from controller.professores_rotas import prof_rotas
 from controller.alunos_rotas import alunos_rotas
 from controller.turmas_rotas import turmas_rotas
 from config import app
-from flask import Flask, jsonify, request
-import database as db
-import datetime
-import re
 
 app.register_blueprint(prof_rotas)
 app.register_blueprint(alunos_rotas)
 app.register_blueprint(turmas_rotas)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000, host="0.0.0.0")
