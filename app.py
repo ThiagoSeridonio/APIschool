eda1e0c968589980b0f58a8d8cd6b2f94997d144
VVPCode <pirotivitor3@gmail.com>
2025-05-03 21:41:08 -0300

    1ª tentativa do Deploy

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from controller.professores_rotas import prof_rotas
 from controller.alunos_rotas import alunos_rotas
 from controller.turmas_rotas import turmas_rotas
 from config import app, db
+import os
 
 app.register_blueprint(prof_rotas)
 app.register_blueprint(alunos_rotas)
@@ -11,4 +12,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host="0.0.0.0")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
