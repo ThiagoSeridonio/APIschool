a3fb6be49258633edad40637e90ab7560adc83d8
VVPCode <pirotivitor3@gmail.com>
2025-05-03 22:42:02 -0300

    Teste Deploy

---
+++ pop/del_alunos.py
@@ -0,0 +1,14 @@
+import requests
+
+# IDs dos alunos que você quer deletar
+ids_para_deletar = [1, 2, 3, 4]  # Altere conforme necessário
+
+url_base = "http://127.0.0.1:5000/alunos"
+
+for aluno_id in ids_para_deletar:
+    url = f"{url_base}/{aluno_id}"
+    response = requests.delete(url)
+    if response.status_code == 200:
+        print(f"Aluno com ID {aluno_id} deletado com sucesso!")
+    else:
+        print(f"Erro ao deletar aluno com ID {aluno_id}: {response.status_code} - {response.text}")
